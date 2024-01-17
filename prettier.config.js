5fc697a3823cc074e7a9fb7198b996c01a7d60eb
Adam Chyb <achybby@gmail.com>
2024-01-17 19:30:19 +1100

    Finished cphapter 5

--- prettier.config.js
+++
@@ -1,6 +0,0 @@
-import vercelConfig, { plugins as vercelPlugins } from '@vercel/style-guide/prettier';
-
-export default {
-  ...vercelConfig,
-  plugins: [...vercelPlugins, 'prettier-plugin-tailwindcss'],
-};
